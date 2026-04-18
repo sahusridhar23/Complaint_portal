d0afdf0f2cfbd5cdc84d708232f0dce6ccd65e8c
sahusridhar23 <sahusridhar23@gmail.com>
2026-04-18 22:24:11 +0530

    update : add AddComplaintServlet.java

--- WEB-INF/src/AddComplaintServlet.java
+++ WEB-INF/src/AddComplaintServlet.java
@@ -10,3 +10,33 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/addComplaint")
+public class AddComplaintServlet extends HttpServlet {
+
+    private Gson json = new Gson();
+
+    protected void do post(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+
+        //get data from frontend
+        string name = req.getParameter("name");
+        string category = req.getParameter("category");
+        string title = req.getParameter("title");
+        string description = req.getParameter("description");
+
+        //create the object (like before)
+        Complaint complaint = new Complaint(name,category,title,description);
+
+        //add the complaint to the arraylist 
+        GetComplaintsServlet.getComplaints().add(complaint);
+
+        //convert to json 
+        String response = json.toJson(complaint);// complaint OBJ -> JSON
+
+        //send back the response
+        PrintWriter out = res.getWriter();
+        out.println(response);
+        out.flush();
+    }
+
+}
