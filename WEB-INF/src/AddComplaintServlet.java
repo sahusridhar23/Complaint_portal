ab4bffbae39bb6d704cf5d424f238ad718bf2b1f
sahusridhar23 <sahusridhar23@gmail.com>
2026-04-19 00:03:08 +0530

    update : add complaintservlet

--- WEB-INF/src/AddComplaintServlet.java
+++ WEB-INF/src/AddComplaintServlet.java
@@ -14,26 +14,26 @@ public class AddComplaintServlet extends HttpServlet {
 
     private Gson json = new Gson();
 
-    protected void do post(HttpServletRequest req, HttpServletResponse res) throws IOException{
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("application/json");
         res.setCharacterEncoding("UTF-8");
 
-        //get data from frontend
-        string name = req.getParameter("name");
-        string category = req.getParameter("category");
-        string title = req.getParameter("title");
-        string description = req.getParameter("description");
+        // get data from frontend
+        String name = req.getParameter("name");
+        String category = req.getParameter("category");
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
 
-        //create the object (like before)
-        Complaint complaint = new Complaint(name,category,title,description);
+        // create the object (like before)
+        Complaint complaint = new Complaint(name, category, title, description);
 
-        //add the complaint to the arraylist 
+        // add the complaint to the arraylist
         GetComplaintsServlet.getComplaints().add(complaint);
 
-        //convert to json 
+        // convert to json
         String response = json.toJson(complaint);// complaint OBJ -> JSON
 
-        //send back the response
+        // send back the response
         PrintWriter out = res.getWriter();
         out.println(response);
         out.flush();
